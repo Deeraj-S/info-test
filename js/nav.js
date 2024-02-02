d65d4c61ce549c9530d9663e10debb33ead4d478
Deeraj-S <hpdeeraj2000@gmail.com>
2024-02-02 22:51:19 +0530

    Made changes

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,19 @@
 const mobile_nav = document.querySelector(".mobile-navbar-btn");
 const nav_header = document.querySelector(".header");
 
+
+
+
 const toggleNavbar = () => {
     nav_header.classList.toggle("active");
+    if (nav_header.classList.contains("active")) {
+        document.getElementsByClassName("container")[0].style.width = "0%"
+    }
+    else {
+        document.getElementsByClassName("container")[0].style.width = "100%";
+
+    }
+
 
 };
 
