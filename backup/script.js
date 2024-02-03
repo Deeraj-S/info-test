184db49efa97b96ee9be388e790746214ff8e179
Deeraj-S <hpdeeraj2000@gmail.com>
2024-02-03 21:56:38 +0530

    Made changes

--- backup/script.js
+++ backup/script.js
@@ -1,20 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Get references to the screen opener, home content, and images
-    const screenOpener = document.getElementById("screenOpener");
-    const home = document.getElementById("home");
-    const image1 = document.getElementById("image1");
-    const image2 = document.getElementById("image2");
-
-    // Show the screen opener for a few seconds
+    // Show screen opener for a few seconds
+    document.getElementById("screenOpener").style.display = "flex";
     setTimeout(function () {
-        // Hide the screen opener
-        screenOpener.style.display = "none";
-
+        document.getElementById("screenOpener").style.display = "none";
         // Show the home content
-        home.style.display = "flex";
-
-        // Fade in images
-        image1.style.opacity = "1";
-        image2.style.opacity = "1";
+        document.getElementById("home").style.display = "block";
     }, 3000); // Adjust the time as needed
 });
